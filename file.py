07aa6b67aa44dd1bf4dbd61fd4b66c39df0a6a86
Егор Тупикин <egor_tupikin02@mail.ru>
2022-07-25 17:25:49 +0300

    start opencv_bot

--- file.py
+++ file.py
@@ -1,3 +1,12 @@
+# import all the libraries for the vk_api
+import vk_api
+from vk_api.longpoll import VkLongPoll , VkEventType
+
+import  _token
+
+# necessary global variables for the chatbot
+group_id = _token.group_id
+token = _token.token
 
 
 
